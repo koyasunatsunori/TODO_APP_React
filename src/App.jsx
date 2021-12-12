78275a19eccdb6a99aad49a81ec5473733730f02
koyasunatsunori <53142478+koyasunatsunori@users.noreply.github.com>
2021-12-13 08:54:53 +0900

    Describe structural HTML with JSX

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,32 @@ import React from "react";
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <div>
+        <input placeholder="ToDoを入力" />
+        <button>追加する</button>
+      </div>
+      <div>
+        <p>未完了のToDo</p>
+        <ul>
+          <li>タスク１</li>
+          <button>完了</button>
+          <button>削除</button>
+        </ul>
+        <ul>
+          <li>タスク2</li>
+          <button>完了</button>
+          <button>削除</button>
+        </ul>
+      </div>
+      <div>
+        <p>完了のToDo</p>
+        <ul>
+          <li>タスク3</li>
+          <button>戻す</button>
+        </ul>
+      </div>
+    </>
+  );
 };
