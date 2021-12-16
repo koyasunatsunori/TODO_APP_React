fca0bdb33ac5d54f564baade6079fa2ae42de489
koyasunatsunori <53142478+koyasunatsunori@users.noreply.github.com>
2021-12-16 21:19:39 +0900

    add return function

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,15 @@ import "./styles.css";
 
 export const App = () => {
   //動的に変化する項目に対いて、stateを割り当てる
+  //stateが複数うある場合、配列で割り当てる
   const [todoText, setTodoText] = useState("");
-  const [incompleteTodos, setIncompleteTodos] = useState([
-    "タスク1",
-    "タスク2"
-  ]);
+  const [incompleteTodos, setIncompleteTodos] = useState([]);
 
-  const [completeTodos, setCompleteTodos] = useState(["タスク3"]);
+  const [completeTodos, setCompleteTodos] = useState([]);
+  //タスクを入力したとき、実行される処理
   const onChangeTodoText = (event) => setTodoText(event.target.value);
 
-  //buttonを押したとき、実行される関数
+  //追加ボタンを押したとき、実行される関数
   const onClickAdd = () => {
     if (todoText === "") return;
     const newTodos = [...incompleteTodos, todoText];
@@ -20,12 +19,14 @@ export const App = () => {
     setTodoText("");
   };
 
+  //削除ボタンを押したとき、実行される関数
   const onClickDelete = (index) => {
     const newTodos = [...incompleteTodos];
     newTodos.splice(index, 1);
     setIncompleteTodos(newTodos);
   };
 
+  //完了ボタンを押したとき、実行される関数
   const onClickComplete = (index) => {
     const newIncompleteTodos = [...incompleteTodos];
     newIncompleteTodos.splice(index, 1);
@@ -33,6 +34,15 @@ export const App = () => {
     const newCompleteTodos = [...completeTodos, incompleteTodos[index]];
     setIncompleteTodos(newIncompleteTodos);
     setCompleteTodos(newCompleteTodos);
+  };
+
+  //戻すボタンを押したとき、実行される関数
+  const onClickBack = (index) => {
+    const newCompleteTodos = [...completeTodos];
+    newCompleteTodos.splice(index, 1);
+    const newIncompleteTodos = [...incompleteTodos, completeTodos[index]];
+    setCompleteTodos(newCompleteTodos);
+    setIncompleteTodos(newIncompleteTodos);
   };
 
   //レンダリングされる内容
@@ -63,11 +73,11 @@ export const App = () => {
       <div className="complete-area">
         <p className="title">完了のToDo</p>
         <ul>
-          {completeTodos.map((todo) => {
+          {completeTodos.map((todo, index) => {
             return (
               <div key={todo} className="list-row">
                 <li>{todo}</li>
-                <button>戻す</button>
+                <button onClick={() => onClickBack(index)}>戻す</button>
               </div>
             );
           })}
